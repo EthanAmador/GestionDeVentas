fca2ee0ca4fe313d868f19bc7888b907e7344916
Gabriel Calvo <gabo.calvo1@gmail.com>
2015-05-27 22:27:21 -0500

    SEPTIMO  committ MODIFICACIÓN de  ProductoDaoImpl

--- src/java/com/gerenciaProyecto/DaoImple/ProductoDaoImpl.java
+++ src/java/com/gerenciaProyecto/DaoImple/ProductoDaoImpl.java
@@ -7,40 +7,66 @@ package com.gerenciaProyecto.DaoImple;
 
 import com.gerenciaProyecto.Dao.ProductoDao;
 import com.gerenciaProyecto.Entites.Producto;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Agandio
  */
-public class ProductoDaoImpl implements ProductoDao{
-    
-     @PersistenceContext
+@Repository
+public class ProductoDaoImpl implements ProductoDao {
+
+    @PersistenceContext
     private EntityManager entityManager;
-     
+
+    EntityManager em = getEntityManager();
+
     public EntityManager getEntityManager() {
         return entityManager;
     }
 
     @Override
     public List<Producto> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        List<Producto> lista = new ArrayList<Producto>();
+        Query q = em.createQuery("SELECT p FROM Producto p where p.estado= :estado ");
+        q.setParameter("estado", Producto.ESTADOS.ACTIVO.getEstado());
+        lista = q.getResultList();
+        return lista;
     }
 
     @Override
     public void crear(Producto producto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            em.persist(producto);
+            em.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Producto> listarInactivos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Producto> lista = new ArrayList<Producto>();
+        Query q = em.createQuery("SELECT p FROM Producto p where p.estado= :estado ");
+        q.setParameter("estado", Producto.ESTADOS.INACTIVO.getEstado());
+        lista = q.getResultList();
+        return lista;
     }
 
     @Override
     public void modificar(Producto producto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            em.merge(producto);
+            em.flush();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+            
     }
 }
