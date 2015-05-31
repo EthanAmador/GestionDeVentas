f1126e5df1d5db8c2178b10afdac459630552089
Gabriel Calvo <gabo.calvo1@gmail.com>
2015-05-31 13:09:50 -0500

    16 commit creación del bean ProductoBeen

--- src/java/com/gerenciaProyecto/Beans/ProductoBean.java
+++ src/java/com/gerenciaProyecto/Beans/ProductoBean.java
@@ -5,14 +5,22 @@
  */
 package com.gerenciaProyecto.Beans;
 
+import com.gerenciaProyecto.Entites.Control;
 import com.gerenciaProyecto.Entites.Producto;
+import com.gerenciaProyecto.Entites.Tienda;
+import com.gerenciaProyecto.Entites.Tipoproducto;
 import com.gerenciaProyecto.Servicio.ProductoService;
+import com.gerenciaProyecto.Util.PanelBean;
+import com.gerenciaProyecto.Util.ValoresSelectOneMenu;
 import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,18 +36,26 @@ public class ProductoBean implements Serializable {
     @Autowired
     private ProductoService productoService;
 
+    @ManagedProperty(value = "#{panelBean}")
+    private PanelBean panelBean;
+
     private List<Producto> productos;
     private List<Producto> productosInactivos;
     private Producto producto;
+    private Control control;
+    private Tienda tienda;
+    private Tipoproducto tipoProducto;
     private String estado;
     private Boolean activo;
     private Boolean inactivo;
     private Boolean nuevo;
     private Boolean listar;
-    
-     private final String reqColor = "redRS";
-    private final String optColor = "greenRS";
 
+    private String idTienda;
+    private String idTipoProducto;
+
+    private final String reqColor = "redRS";
+    private final String optColor = "greenRS";
 
     /**
      * Creates a new instance of ProductoBean
@@ -56,6 +72,7 @@ public class ProductoBean implements Serializable {
             inactivo = false;
 
             producto = new Producto();
+            control = new Control(3, Control.ESTADOS.ACTIVO.getEstado());
             productos = productoService.listar();
             productosInactivos = productoService.listarInactivos();
         } catch (Exception e) {
@@ -66,16 +83,32 @@ public class ProductoBean implements Serializable {
     //<editor-fold defaultstate="collapsed" desc="action">
     public void onCrear(ActionEvent ev) {
         producto.setEstado(Producto.ESTADOS.ACTIVO.getEstado());
-//        if (control != null) {
-//            producto.setIdControl(control);
-//        } else {
-//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, " Informacion:  ", "No se ha encontrado registro de control"));
-//        }
-//        try {
-//            productoService.crear(producto);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+
+        if (control != null) {
+            producto.setIdControl(control);
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, " Informacion:  ", "No se ha encontrado registro de control o de tiedas"));
+        }
+
+        if (getIdTienda() != null || !getIdTienda().isEmpty()) {
+            tienda = new Tienda(Integer.parseInt(getIdTienda()), Tienda.ESTADOS.ACTIVO.getEstado());
+            producto.getIdTienda();
+        } else {
+            panelBean.showWarning("Debe selecionar una tienda para asociar el producto");
+        }
+
+        if (getIdTipoProducto() != null || !getIdTipoProducto().isEmpty()) {
+            tipoProducto = new Tipoproducto(Integer.parseInt(getIdTipoProducto()), estado, Producto.ESTADOS.ACTIVO.getEstado());
+        } else {
+            panelBean.showWarning("Debe selecionar una tipo de producto");
+        }
+        producto.getIdTipoproducto();
+
+        try {
+            productoService.crear(producto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         productos = productoService.listar();
         producto = new Producto();
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro :  ", "Creado de forma exitosa"));
@@ -145,8 +178,6 @@ public class ProductoBean implements Serializable {
     public void setProductoService(ProductoService productoService) {
         this.productoService = productoService;
     }
-    
-        
 
     public List<Producto> getProductosInactivos() {
         return productosInactivos;
@@ -203,5 +234,77 @@ public class ProductoBean implements Serializable {
     public void setListar(Boolean listar) {
         this.listar = listar;
     }
+
+    public Control getControl() {
+        return control;
+    }
+
+    public void setControl(Control control) {
+        this.control = control;
+    }
+
+    public String getIdTienda() {
+        return idTienda;
+    }
+
+    public void setIdTienda(String idTienda) {
+        this.idTienda = idTienda;
+    }
+
+    public String getIdTipoProducto() {
+        return idTipoProducto;
+    }
+
+    public void setIdTipoProducto(String idTipoProducto) {
+        this.idTipoProducto = idTipoProducto;
+    }
+
+    public Tienda getTienda() {
+        return tienda;
+    }
+
+    public void setTienda(Tienda tienda) {
+        this.tienda = tienda;
+    }
+
+    public Tipoproducto getTipoProducto() {
+        return tipoProducto;
+    }
+
+    public void setTipoProducto(Tipoproducto tipoProducto) {
+        this.tipoProducto = tipoProducto;
+    }
+
+    public String getReqColor() {
+        return reqColor;
+    }
+
+    public String getOptColor() {
+        return optColor;
+    }
+
     //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="itemList">
+    public List<SelectItem> onListTienda() {
+        List<SelectItem> list = new LinkedList<SelectItem>();
+        try {
+            list = ValoresSelectOneMenu.getListTipoTienda();
+        } catch (Exception e) {
+//            e.printStackTrace();
+            panelBean.showError("Error al cargar las tiendas");
+        }
+        return list;
+    }
+
+    public List<SelectItem> onListTipoProducto() {
+        List<SelectItem> list = new LinkedList<SelectItem>();
+        try {
+            list = ValoresSelectOneMenu.getListTipoProducto();
+        } catch (Exception e) {
+//            e.printStackTrace();
+            panelBean.showError("Error al cargar las tipos de productos");
+        }
+        return list;
+    }
+//</editor-fold>
 }
